e660cd6f1eb22d84e65bddbdd3bb9ce5aac23fbc
Vedad Sose <vedadsose@gmail.com>
2019-09-12 12:28:43 +0200

    Improve deployment slack message

--- commands/publish-deploy.js
+++ commands/publish-deploy.js
@@ -10,6 +10,7 @@ module.exports = {
       })
       .option('env', { describe: 'deployment environment' })
       .option('image', { describe: 'docker image deployed' })
+      .option('deployer', { describe: 'github username of deployer' })
       .demandOption(
         ['app', 'env'],
         'Please provide both app and env arguments'
@@ -18,27 +19,36 @@ module.exports = {
     const endPoint =
       'https://hooks.slack.com/services/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'
 
+    const { data: deployer } = await axios.get(
+      `https://api.github.com/users/${args.deployer}`
+    )
+
     const body = {
-      text: 'Ok',
-      blocks: [
+      attachments: [
         {
-          type: 'section',
-          text: {
-            type: 'plain_text',
-            text: 'Deployment successful :white_check_mark:',
-            emoji: true
-          }
-        },
-        {
-          type: 'context',
-          elements: [
-            { type: 'mrkdwn', text: `*Application:* ${args.app}` },
-            { type: 'mrkdwn', text: `*Environment:* ${args.env}` },
+          text: 'Successful deployment',
+          color: '#36a64f',
+          author_name: deployer.name,
+          author_icon: deployer.avatar_url,
+          fields: [
+            {
+              title: 'Application',
+              value: args.app,
+              short: false
+            },
+            {
+              title: 'Environment',
+              value: args.env,
+              short: false
+            },
             args.image && {
-              type: 'mrkdwn',
-              text: `*Image*: ${args.image}`
+              title: 'Image',
+              value: args.image,
+              short: false
             }
-          ].filter(Boolean)
+          ].filter(Boolean),
+          footer: 'UBDI-CLI',
+          ts: Math.round(new Date().getTime() / 1000)
         }
       ]
     }
